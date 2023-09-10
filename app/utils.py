9340dace99495fe7f8b42f1255fd915e3ecf057d
breno-jesus-fernandes <breno.de.jesus.fernandes@gmail.com>
2023-09-09 21:55:52 -0300

    FEAT: Add hyperparameter

--- app/utils.py
+++ app/utils.py
@@ -4,6 +4,7 @@ from typing import Any, Dict
 import dill
 from loguru import logger
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 @logger.catch
@@ -16,22 +17,32 @@ def save_object(file_path: Path, obj: Any):
 
 @logger.catch
 def evaluate_models(
-    X_train, y_train, X_test, y_test, models: Dict[str, Any]
+    X_train,
+    y_train,
+    X_test,
+    y_test,
+    models: Dict[str, Any],
+    params: Dict[str, Any],
 ) -> Dict[str, float]:
     """
     This function is responsible for evaluate de r2 score from different ML models
     """
 
     report: Dict[str, float] = {}
-    for name, model in models.items():
+    for (model_name, model), (_, param) in zip(models.items(), params.items()):
+        gs = GridSearchCV(model, param, cv=3, n_jobs=3)
+        gs.fit(X_train, y_train)
+
+        model.set_params(**gs.best_params_)
         model.fit(X_train, y_train)
+
         y_train_pred = model.predict(X_train)
         y_test_pred = model.predict(X_test)
 
         train_model_score = r2_score(y_train, y_train_pred)
         test_model_score = r2_score(y_test, y_test_pred)
 
-        logger.info(f'Model: {name}, R2 SCORE: {test_model_score:.2f}')
-        report[name] = float(test_model_score)
+        logger.info(f'Model: {model_name}, R2 SCORE: {test_model_score:.2f}')
+        report[model_name] = float(test_model_score)
 
     return report
